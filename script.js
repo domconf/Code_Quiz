8c266b035f0bcb6e6cb5051d21e33b245ec622e9
Dominick Confessore <domconfessore@gmail.com>
2023-03-20 16:00:49 -0400

    declared variables for start/info section

--- script.js
+++ script.js
@@ -1,69 +1,79 @@
-const quizContainer = document.getElementById("quiz-container");
-const questionContainer = document.getElementById("question");
-const choicesContainer = document.getElementById("choices");
-const startButton = document.getElementById("start-btn");
-const timer = document.getElementById("timer");
-const timeLeftContainer = document.getElementById("time-left");
-const submitButton = document.getElementById("submit-btn");
-let currentQuestion = 0;
-let timeLeft = 60;
+// Start Section
+let start = document.querySelector("#start");
 
-const questions = [
-    {
-        question: "What is the term used to describe the NCAAB tournament?",
-        choices: ["April Angerness", "March Madness", "May Madness", "June Joyous"],
-        correctAnswer: "March Madness",
-    },
-    {
-        question: "Which school has the most national championships in college basketball?",
-        choices: ["Duke", "Kentucky", "UCLA", "North Carolina"],
-        correctAnswer: "UCLA",
-    },
-    {
-        question: "Including play-in games, how many teams make the NCAAB tournament?",
-        choices: ["68", "64", "60", "72"],
-        correctAnswer: "68",
-    },
-    {
-        question: "Which school did Michael Jordan play college basketball at?",
-        choices: ["Michigan", "Duke", "Kentucky", "North Carolina"],
-        correctAnswer: "North Carolina",
-    },
-    {
-        question: "What was the nickame for Michigan's team during the 1991 NCAAB season?",
-        choices: ["Fantastic Four", "Flawless Five", "Fab Five", "Fit Five"],
-        correctAnswer: "Fab Five",
-    },
-    {
-        question: "Which coach has the most wins in NCAAB history?",
-        choices: ["Jim Boeheim", "Mike Krzyzewski", "Roy Williams", "Bob Knight"],
-        correctAnswer: "Mike Krzyzewski",
-    },
-    {
-        question: "Which coach had the iconic speech at the ESPY's stating 'don't give up, don't ever give up?'",
-        choices: ["Jim Valvano", "Jim Boeheim", "Lou Carnasecca", "Mike Krzyzewski"],
-        correctAnswer: "Jim Valvano",
-    },
-    {
-        question: "Which former Duke player has the most points in school history?",
-        choices: ["Christian Laettner", "Kyle Singler", "Jon Scheyer", "JJ Redick"],
-        correctAnswer: "JJ Redick",
-    },
-    {
-        question: "Who was the first 16 seed to beat a number 1 seed in the NCAAB tournament?",
-        choices: ["Fairleigh Dickinson University", "University of Maryland, Baltimore County", "Florida Gulf Coast University", "Howard University"],
-        correctAnswer: "University of Maryland, Baltimore County",
-    },
-    {
-        question: "What was the lowest seed to ever win the NCAAB championship?",
-        choices: ["8 seed", "6 seed", "10 seed", "12 seed"],
-        correctAnswer: "8 seed",
-    }
-];
+// Info Section
+let info = document.querySelector("#info");
+let exit = document.querySelector("#exit");
+let continueBtn = document.querySelector("#continue");
+
+// Quiz Section
+
+// const quizContainer = document.getElementById("quiz-container");
+// const questionContainer = document.getElementById("question");
+// const choicesContainer = document.getElementById("choices");
+// const startButton = document.getElementById("start-btn");
+// const timer = document.getElementById("timer");
+// const timeLeftContainer = document.getElementById("time-left");
+// const submitButton = document.getElementById("submit-btn");
+// let currentQuestion = 0;
+// let timeLeft = 60;
+
+// const questions = [
+//     {
+//         question: "What is the term used to describe the NCAAB tournament?",
+//         choices: ["April Angerness", "March Madness", "May Madness", "June Joyous"],
+//         correctAnswer: "March Madness",
+//     },
+//     {
+//         question: "Which school has the most national championships in college basketball?",
+//         choices: ["Duke", "Kentucky", "UCLA", "North Carolina"],
+//         correctAnswer: "UCLA",
+//     },
+//     {
+//         question: "Including play-in games, how many teams make the NCAAB tournament?",
+//         choices: ["68", "64", "60", "72"],
+//         correctAnswer: "68",
+//     },
+//     {
+//         question: "Which school did Michael Jordan play college basketball at?",
+//         choices: ["Michigan", "Duke", "Kentucky", "North Carolina"],
+//         correctAnswer: "North Carolina",
+//     },
+//     {
+//         question: "What was the nickame for Michigan's team during the 1991 NCAAB season?",
+//         choices: ["Fantastic Four", "Flawless Five", "Fab Five", "Fit Five"],
+//         correctAnswer: "Fab Five",
+//     },
+//     {
+//         question: "Which coach has the most wins in NCAAB history?",
+//         choices: ["Jim Boeheim", "Mike Krzyzewski", "Roy Williams", "Bob Knight"],
+//         correctAnswer: "Mike Krzyzewski",
+//     },
+//     {
+//         question: "Which coach had the iconic speech at the ESPY's stating 'don't give up, don't ever give up?'",
+//         choices: ["Jim Valvano", "Jim Boeheim", "Lou Carnasecca", "Mike Krzyzewski"],
+//         correctAnswer: "Jim Valvano",
+//     },
+//     {
+//         question: "Which former Duke player has the most points in school history?",
+//         choices: ["Christian Laettner", "Kyle Singler", "Jon Scheyer", "JJ Redick"],
+//         correctAnswer: "JJ Redick",
+//     },
+//     {
+//         question: "Who was the first 16 seed to beat a number 1 seed in the NCAAB tournament?",
+//         choices: ["Fairleigh Dickinson University", "University of Maryland, Baltimore County", "Florida Gulf Coast University", "Howard University"],
+//         correctAnswer: "University of Maryland, Baltimore County",
+//     },
+//     {
+//         question: "What was the lowest seed to ever win the NCAAB championship?",
+//         choices: ["8 seed", "6 seed", "10 seed", "12 seed"],
+//         correctAnswer: "8 seed",
+//     }
+// ];
 
 
-startButton.addEventListener('click', startQuiz);
+// startButton.addEventListener('click', startQuiz);
 
-function startQuiz() {
-    startButton
-}
+// function startQuiz() {
+//     startButton
+// }
