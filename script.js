9ce4ebd875bc260defca2c365a68d4f3ce5935f5
Dominick Confessore <domconfessore@gmail.com>
2023-03-20 16:21:20 -0400

    Declared variables and added click events

--- script.js
+++ script.js
@@ -20,11 +20,41 @@ let option2 = document.querySelector("#option2");
 let option3 = document.querySelector("#option3");
 let option4 = document.querySelector("#option4");
 
+// Buttons
+let total_correct = document.querySelector("#total_correct");
+let next_question = document.querySelector("#next_question");
 
+// Result Section
+let result = document.querySelector("#result")
+let points = document.querySelector("#points");
+let quit = document.querySelector("quit");
+let startAgain = document.querySelector("#startAgain");
 
+// Get all H4 from quiz section
+let choice_que = document.querySelector(".choice_que");
 
+let index = 0;
+let timer = 0;
+let interval = 0;
 
+// Total Points
+let correct = 0;
 
+//Store Answer
+let UserAns = undefined;
+
+start.addEventListener("click", () => {
+    start.style.display = "none";
+    info.style.display = "block";
+});
+
+exit.addEventListener("click", () => {
+    start.style.display = "block";
+    info.style.display = "none";
+});
+
+let countDown = () => {
+}
 
 
 
